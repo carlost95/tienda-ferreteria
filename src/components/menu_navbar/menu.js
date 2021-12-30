91b1326fb3fb2ac653e5aef5764324a8545479ab
carlost95 <carlosanibaltejada@gmail.com>
2021-12-29 21:56:12 -0300

    agregado de menu sin formatos

---
+++ src/components/menu_navbar/menu.js
@@ -0,0 +1,21 @@
+export const NavBar = () => {
+    return (
+      <nav className="navBar">
+        <p>Ferrerteria Josecito</p>
+        <ul>
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">productos</a>
+          </li>
+          <li>
+            <a href="#">servicios </a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    );
+};
